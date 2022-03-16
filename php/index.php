4bfbd49096afd91e89a5a62be7097b5e6975cff5
Alex <92427885+AlexAlessandria30@users.noreply.github.com>
2022-03-16 11:46:57 +0100

    modifiche esercitazioni

--- php/index.php
+++ php/index.php
@@ -1,14 +1,34 @@
 <?php
 
-$numero = [12,12,1,3];
+// $numero = [3,3,4,12,12,12,12,121,2654,3211,298776];
+
+// for ($i = 0; $i < count($numero); $i++) {
+//     if ($numero[$i] % 2 == 0) {
+//         $somma = array_sum($numero);
+//         $media = $somma / count($numero);
+
+//         echo "La media è: " . $media . "e la somma è " . $somma . "\n";
+//     }else {
+//         echo $numero[$i] ." è numeri sono dispari \n";
+//     }
+// }
 
 
-if ($numero % 2 == 0 ) {
-    $somma = array_sum($numero);
-    $media = $somma / count($numero);
+$persona = [
+    'nome' => 'Mario',
+    'cognome' => 'Rossi',
+    'genere' => 'M',
+];
 
-    echo "La media è: " . $media . "e la somma è " . $somma;
-};
+
+
+foreach ($persona as $chiave => $valore) {
+    //echo $chiave . "=>" . $valore . "\n";
+}
+
+
+
+
 
 
 $persone = [
@@ -38,39 +58,42 @@ $persone = [
     ],
 ];
 
-// print_r($persone);
+//  print_r($persone);
+// echo $persone[0]['nome'] . "\n";
+// echo $persone[1]['nome'] . "\n";
+// echo $persone[2]['nome'] . "\n";
 
 
-// for ($i=0; $i < count($persone[$i]); $i++) { 
+for ($i = 0; $i < count($persone[$i]); $i++) {
 
-//   if ($persone['genere' == 'M'] ) {
-//     echo  'Buongiorno Sig.' .$persone['nome']. " " .$persone['cognome']. "\n";
-// }elseif ($persone['genere' == 'F'] ) {
-//     echo  'Buongiorno Sig.ra' .$persone['nome']. " " .$persone['cognome']. "\n";
-// }else {
-//     echo "Buongiorno \n";
-// }
-// }
-
-
+    if ($persone[$i]['genere'] == "M") {
+        echo  'Buongiorno Sig.  ' . $persone[$i]['nome'] . " " . $persone[$i]['cognome'] . "\n";
+    } elseif ($persone[$i]['genere'] == 'F') {
+        echo  'Buongiorno Sig.ra  ' . $persone[$i]['nome'] . " " . $persone[$i]['cognome'] . "\n";
+    } else {
+        echo "Buongiorno \n";
+    }
+}
 
 
-// function FizzBuzz($n){
-
-// for ($i = 0; $i <= $n; $i++) {
-
-//     if ($i % 3 == 0 && $i % 5 == 0) {
-//         echo ('HACKADEMY35');
-//     } elseif ($i % 3 == 0) {
-//         echo ('PHP');
-//     } else if ($i % 5 == 0) {
-//         echo ('JAVASCRIPT');
-//     } else {
-//         echo ($i);
-//     }
-//     echo "\n";
-// };
-// }
 
 
-// FizzBuzz(20);
+function FizzBuzz($n){
+
+for ($i = 0; $i <= $n; $i++) {
+
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo ('fizz');
+    } elseif ($i % 3 == 0) {
+        echo ('buzz');
+    } else if ($i % 5 == 0) {
+        echo ('fizzbuzz');
+    } else {
+        echo ($i);
+    }
+    echo "\n";
+};
+}
+
+
+//FizzBuzz(12345332);
--- php/prova.php
+++ php/prova.php
@@ -1,66 +1,80 @@
 <?php
 
-echo "ciao \n";
 
-$a = "Ciao ";
-$b = "Giuseppe \n";
-
-
-echo  $a  . $b;
-
-
-$arr = [1, 2, 3, 4];
-
-for ($i = 0; $i < count($arr); $i++) {
-
-    // echo $arr[$i] ."\n" ;
-
-    echo "$arr[$i] \n";
+$eta_utenti = array(
+    'Simone' => 29,
+    'Josephine' => 30,
+    'Giuseppe' => 23,
+    'Renato' => 26,
+    'Gabriele' => 24
+);
+foreach ($eta_utenti as $nome => &$eta) {
+    $eta++;
 }
+print_r($eta_utenti);
 
-class Saluto
-{
 
-    public $nome;
-    public $cognome;
+// echo "ciao \n";
 
-    public function __construct($nome, $cognome)
-    {
-        $this->nome = $nome;
-        $this->cognome = $cognome;
-    }
+// $a = "Ciao ";
+// $b = "Giuseppe \n";
 
-    public  function print()
-    {
 
-        echo "Ciao $this->nome $this->cognome \n";
-    }
-}
+// echo  $a  . $b;
 
-$Giuseppe = new Saluto("Giuseppe", "Rossi");
 
-print_r($Giuseppe);
+// $arr = [1, 2, 3, 4];
 
-$Giuseppe->print();
+// for ($i = 0; $i < count($arr); $i++) {
 
-class Cammino extends Saluto
-{
+   
 
-    public $luogo;
+//     echo "$arr[$i] \n";
+// }
 
-    public function __construct($nome, $cognome, $luogo)
-    {
-        Saluto::__construct($nome, $cognome);
-        $this->luogo = $luogo;
-    }
+// class Saluto
+// {
 
-    public  function luogo()
-    {
+//     public $nome;
+//     public $cognome;
 
-        echo "Ciao sono  $this->nome $this->cognome e sto andando $this->luogo \n";
-    }
-}
+//     public function __construct($nome, $cognome)
+//     {
+//         $this->nome = $nome;
+//         $this->cognome = $cognome;
+//     }
 
-$Francesco = new Cammino("Francesco", "Blu", "Montagna");
+//     public  function print()
+//     {
 
-$Francesco->luogo();
+//         echo "Ciao $this->nome $this->cognome \n";
+//     }
+// }
+
+// $Giuseppe = new Saluto("Giuseppe", "Rossi");
+
+// print_r($Giuseppe);
+
+// $Giuseppe->print();
+
+// class Cammino extends Saluto
+// {
+
+//     public $luogo;
+
+//     public function __construct($nome, $cognome, $luogo)
+//     {
+//         Saluto::__construct($nome, $cognome);
+//         $this->luogo = $luogo;
+//     }
+
+//     public  function luogo()
+//     {
+
+//         echo "Ciao sono  $this->nome $this->cognome e sto andando $this->luogo \n";
+//     }
+// }
+
+// $Francesco = new Cammino("Francesco", "Blu", "Montagna");
+
+// $Francesco->luogo();
